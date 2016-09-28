7442c3aca12935a30e7f52c087e989f28bd18237
Bolun Zhang <rzhangbolun@gmail.com>
2016-09-28 00:40:39 -0400

    improve speed

--- algorithms/387_first_unique_character_in_a_string/solution.cpp
+++ algorithms/387_first_unique_character_in_a_string/solution.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int firstUniqChar(string s)
@@ -16,11 +17,38 @@ public:
     }
 };
 
-/*
  104 / 104 test cases passed.
  Status: Accepted
  Runtime: 58 ms
  Submitted: 1 hour, 52 minutes ago
  You are here!
  Your runtime beats 52.67% of cpp submissions.
+*/
+
+class Solution {
+public:
+    int firstUniqChar(string s)
+    {
+        // fix char size
+        int map[256] = {0};
+        for(int i=0;i<s.size();++i)
+        {
+            // IMPORTANT: pre-increment significantly improved speed
+            ++map[s[i]];
+        }
+        for(int i=0;i<s.size();++i)
+        {
+            if(map[s[i]]==1)
+                return i;
+        }
+        return -1;
+    }
+};
+
+/*
+ 104 / 104 test cases passed.
+ Status: Accepted
+ Runtime: 42 ms
+ You are here!
+ Your runtime beats 92.83% of cpp submissions.
 */
