504504b1a4e8517ef84ab438ffe3a9f889c23a30
Bolun Zhang <rzhangbolun@gmail.com>
2018-06-13 15:41:33 -0400

    weirdo error but works

--- algorithms/146_lru_cache/solution.cpp
+++ algorithms/146_lru_cache/solution.cpp
@@ -1,23 +1,45 @@
+
 class LRUCache {
 public:
+
+    struct Node;
+
     LRUCache(int capacity) {
-        capacity_ = capacity;    
+        capacity_ = capacity;
+        size_ = 0;
         head_ = NULL;
         tail_ = NULL;
+    }
+    ~LRUCache()
+    {
+        unordered_map<int,Node*>::iterator it;
+        for( it = keyToNode_.begin(); it != keyToNode_.end(); ++it )
+        {
+            delete (it->second);
+            keyToNode_.erase( it->first );
+        }
     }
 
     int capacity_;
     int size_;
     Node* head_;
     Node* tail_;
-    unordered_map<int,int> keyToNode_;
+    unordered_map<int,Node*> keyToNode_;
 
     struct Node
     {
+        Node( int k, int v )
+        : prev( NULL )
+        , next( NULL )
+        , key(k)
+        , value(v)
+        {}
+
         Node* prev;
         Node* next;
+        int key;
         int value;
-    }
+    };
 
     Node* getNode( int key )
     {
@@ -31,13 +53,12 @@ public:
             return NULL;
         }
     }
-    
+
     int get(int key) {
         Node* np = getNode(key);
-        if( np!= NULL )
+        if( np )
         {
-            // update frequency
-            
+            touchNode(np);
             return np->value;
         }
         else
@@ -45,19 +66,87 @@ public:
             return -1;
         }
     }
-    
-    void put(int key, int value) {
-        Node* np = getNode(key);
-        if( np != NULL )
-        {
-            //already in double link list, move to head
-            Node* np= (it->second);
-            Node* temp = np->next;
-            np->next = np->prev;
-            np->prev = temp;
 
-            head_ = 
+    void put(int key, int value) {
+
+        if( capacity_ <= 0 )
+            return;
+
+        Node* np = getNode(key);
+        if( np )
+        {
+            // already in double link list, move to head, no addition needed, no removal needed
+            touchNode(np);
         }
+        else
+        {
+            // not in double link list, create node, insert in front, remove from tail if need
+            np = new Node( key, value );
+            insertNode( np );
+            removeIfNeed();
+        }
+    }
+
+    void insertNode( Node* nodeToInsert )
+    {
+        if( !size_ )
+            tail_ = nodeToInsert;
+        if( head_ )
+            head_->prev = nodeToInsert;
+
+        nodeToInsert->prev = NULL;
+        nodeToInsert->next = head_;
+        head_ = nodeToInsert;
+
+        keyToNode_[ nodeToInsert->key ] = nodeToInsert;
+        ++size_;
+        return;
+    }
+
+    void removeIfNeed()
+    {
+        if( size_ > capacity_ )
+        {
+            keyToNode_.erase( tail_->key );
+            tail_ = tail_->prev;
+            
+            // tail should never be null here
+            delete (tail_->next);
+            tail_->next = NULL;
+        }
+    }
+
+    void touchNode( Node* nodeToTouch )
+    {
+        if( size_ == 1 )
+            return;
+
+        // if node is not head
+        if( nodeToTouch->prev != NULL )
+        {
+            nodeToTouch->prev->next = nodeToTouch->next;
+        }
+
+        // if node is not tail
+        if( nodeToTouch->next != NULL )
+        {
+            nodeToTouch->next->prev = nodeToTouch->prev;
+        }
+
+        if( nodeToTouch == tail_ )
+        {
+            tail_ = nodeToTouch->prev;
+        }
+
+        if( nodeToTouch != head_ )
+        {
+            // head should never be null here
+            head_->prev = nodeToTouch;
+            nodeToTouch->next = head_;
+            nodeToTouch->prev = NULL;
+            head_ = nodeToTouch;
+        }
+
     }
 };
 
@@ -67,3 +156,4 @@ public:
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+
