95c1789a88d63879e2f163e7c899d3035e625ef5
Bolun <rzhangbolun@gmail.com>
2018-08-13 02:24:50 -0400

    kmp

--- lintcode/13_implement_strstr/solution.cpp
+++ lintcode/13_implement_strstr/solution.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     /*
-     * @param source: source string to be scanned.
-     * @param target: target string containing the sequence of characters to match
-     * @return: a index to the first occurrence of target in source, or -1  if target is not part of source.
+     * @param source: A source string
+     * @param target: A target string
+     * @return: An integer as index
      */
-    int strStr(const char *source, const char *target) {
+    int strStr(const char* source, const char* target) {
         if( target != NULL && target[0] == '\0' )
             return 0;
         if( target == NULL || source == NULL || source[0] == '\0' )
             return -1;
         
         buildKmp( target );
-        for( int i = 0, j = 0; source[i] != '\0'; )
+        for( int i = 0, j = 0; source[i] != '\0' && target[j] != '\0'; )
         {
             if( source[i] == target[j] )
             {
                 ++i;++j;
                 if( target[j] == '\0' )
                 {
-                    return i-j;
+                    return i - j;
                 }
             }
             else if( j == 0 )
@@ -28,7 +28,7 @@ public:
             }
             else
             {
-                j = kmp[i-1];
+                j = kmp[j-1];
             }
         }
         return -1;
@@ -36,18 +36,29 @@ public:
     
     std::vector<int> kmp;
     
+    int len( const char* target )
+    {
+        int length = 0;
+        
+        while( target[length] != '\0' )
+            ++length;
+        
+        return length;
+    }
+    
     void buildKmp(const char * target)
     {
+        kmp.assign( len(target), 0 );
         for( int j = 0, i = 1; target[i] != '\0'; )
         {
             if( target[i] == target[j] )
             {
-                kmp.push_back(j+1);
+                kmp[i] = j + 1;
                 ++i;++j;
             }
             else if( j == 0 )
             {
-                kmp.push_back(0);
+                kmp[i] = 0;
                 ++i;
             }
             else
