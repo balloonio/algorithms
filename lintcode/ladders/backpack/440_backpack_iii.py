b9055b2a2b42587068a09b80eaa5b3a49986c773
Bolun Zhang <rzhangbolun@gmail.com>
2018-09-12 18:30:43 -0400

    add backpack

---
+++ lintcode/ladders/backpack/440_backpack_iii.py
@@ -0,0 +1,26 @@
+class Solution:
+    """
+    @param A: an integer array
+    @param V: an integer array
+    @param m: An integer
+    @return: an array
+    """
+    def backPackIII(self, A, V, m):
+        # write your code here
+        if not A or not V or not m:
+            return 0
+        
+        n = len(A)    
+        # f[i][j] = max value for first i items with j weight
+        f = [ [0]*(m+1) for _ in range(n+1)]
+        
+        result = 0
+        for i in range(1, n+1):
+            for j in range(m+1):
+                f[i][j] = f[i-1][j]
+                f[i][j] = max(f[i][j], f[i][j-A[i-1]] + V[i-1]) if j-A[i-1] >= 0 else f[i][j]
+                
+                if i == n:
+                    result = max(result, f[i][j])
+        
+        return result
---
+++ lintcode/ladders/backpack/563_backpack_v.py
@@ -0,0 +1,25 @@
+class Solution:
+    """
+    @param nums: an integer array and all positive numbers
+    @param target: An integer
+    @return: An integer
+    """
+    def backPackV(self, nums, target):
+        # write your code here
+        if not nums:
+            return 0
+            
+        n = len(nums)
+        
+        # f[i][j] = number of ways to fill backpack with first i items and target j 
+        f = [0]*(target+1)
+        
+        for i in xrange(target+1):
+            f[i] = 0
+        f[0] = 1 
+            
+        for i in xrange(1, n+1):
+            for j in reversed(range(target+1)):
+                f[j] = f[j] + ( f[j-nums[i-1]] if j-nums[i-1] >= 0 else 0)
+        
+        return f[-1]
