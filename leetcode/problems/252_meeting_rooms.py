62ac1eebc6fe80a6695369e8ffb183f269a53535
Bolun Zhang <rzhangbolun@gmail.com>
2018-09-18 02:55:19 -0400

    added black; added meeting room

---
+++ leetcode/problems/252_meeting_rooms.py
@@ -0,0 +1,33 @@
+# Definition for an interval.
+# class Interval:
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+
+class Solution:
+    def canAttendMeetings(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: bool
+        """
+        if not intervals:
+            return True
+
+        events = []
+        for interval in intervals:
+            start, end = interval.start, interval.end
+            events += [(start, 1)]
+            events += [(end, 0)]
+        events.sort()
+
+        max_meeting = 0
+        curr_meeting = 0
+        for event in events:
+            time, is_start = event
+            if is_start:
+                curr_meeting += 1
+            else:
+                curr_meeting -= 1
+            max_meeting = max(max_meeting, curr_meeting)
+        return max_meeting <= 1
---
+++ lintcode/ladders/system_design/538_memcache.py
@@ -0,0 +1,87 @@
+class Memcache:
+    def __init__(self):
+        # do intialization if necessary
+        self.key2value = {}
+        self.key2expire = {}
+        self.expire2keys = collections.defaultdict(set)
+
+    """
+    @param: curtTime: An integer
+    @param: key: An integer
+    @return: An integer
+    """
+    def get(self, curtTime, key):
+        # write your code here
+        self.check_expire(curtTime)
+        if key not in self.key2value:
+            return 2147483647
+        return self.key2value[key]
+
+    """
+    @param: curtTime: An integer
+    @param: key: An integer
+    @param: value: An integer
+    @param: ttl: An integer
+    @return: nothing
+    """
+    def set(self, curtTime, key, value, ttl):
+        # write your code here
+        self.check_expire(curtTime)
+        self.delete(curtTime, key)
+        self.key2value[key] = value
+        if ttl:
+            self.expire2keys[curtTime+ttl].add(key)
+            self.key2expire[key] = curtTime+ttl
+    """
+    @param: curtTime: An integer
+    @param: key: An integer
+    @return: nothing
+    """
+    def delete(self, curtTime, key):
+        # write your code here
+        self.check_expire(curtTime)
+        if key not in self.key2value:
+            return
+        self.key2value.pop(key)
+        if key not in self.key2expire:
+            return
+        exp = self.key2expire.pop(key)
+        self.expire2keys[exp].remove(key)
+
+    """
+    @param: curtTime: An integer
+    @param: key: An integer
+    @param: delta: An integer
+    @return: An integer
+    """
+    def incr(self, curtTime, key, delta):
+        # write your code here
+        self.check_expire(curtTime)
+        if key not in self.key2value:
+            return 2147483647
+        self.key2value[key] += delta
+        return self.key2value[key]
+    """
+    @param: curtTime: An integer
+    @param: key: An integer
+    @param: delta: An integer
+    @return: An integer
+    """
+    def decr(self, curtTime, key, delta):
+        # write your code here
+        self.check_expire(curtTime)
+        if key not in self.key2value:
+            return 2147483647
+        self.key2value[key] -= delta
+        return self.key2value[key]
+
+    def check_expire(self, curtTime):
+        if curtTime in self.expire2keys:
+            keys = self.expire2keys[curtTime]
+            for key in keys:
+                self.key2value.pop(key)
+                self.key2expire.pop(key)
+            self.expire2keys.pop(curtTime)
+
+# pay attention that set key can refresh time
+# pay attention that key might not have an entry in key2expire
