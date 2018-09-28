0cac4033fda2e695af50518ed6b0f7b722f00614
Bolun Zhang <rzhangbolun@gmail.com>
2018-09-27 23:47:38 -0400

    added linear solution for one edit distance

--- leetcode/problems/161_one_edit_distance.py
+++ leetcode/problems/161_one_edit_distance.py
@@ -10,30 +10,35 @@ class Solution(object):
         if not s or not t:
             return True if abs(len(s) - len(t)) == 1 else False
 
-        # f[i][j] = min dist between s[:i] and t[:t]
         slen, tlen = len(s), len(t)
-        f = [[0] * (tlen + 1) for _ in range(slen + 1)]
+        if abs(slen - tlen) > 1:
+            return False
 
-        for i in range(slen + 1):
-            for j in range(tlen + 1):
-                if i == 0 and j == 0:
-                    continue
-                f[i][j] = math.inf
-                # same char, same as before
-                if i - 1 >= 0 and j - 1 >= 0 and s[i - 1] == t[j - 1]:
-                    f[i][j] = min(f[i][j], f[i - 1][j - 1])
-                # different char
-                if i - 1 >= 0 and j - 1 >= 0 and s[i - 1] != t[j - 1]:
-                    f[i][j] = min(f[i][j], f[i - 1][j - 1] + 1)
-                # by s[:i-1] adding 1 char to meet t[:j]
-                if i - 1 >= 0:
-                    f[i][j] = min(f[i][j], f[i - 1][j] + 1)
-                # by t[:j-1] adding 1 char to meet s[:i]
-                if j - 1 >= 0:
-                    f[i][j] = min(f[i][j], f[i][j - 1] + 1)
+        sidx, tidx = 0, 0
+        modcnt = 0
+        while sidx < slen and tidx < tlen:
+            if s[sidx] == t[tidx]:
+                sidx += 1
+                tidx += 1
+                continue
+            if modcnt == 1:
+                return False
+            modcnt += 1
+            if slen == tlen:
+                sidx += 1
+                tidx += 1
+                continue
+            if slen == tlen - 1:
+                tidx += 1
+            else:
+                sidx += 1
 
-        return f[slen][tlen] == 1
+        modcnt += slen - sidx + tlen - tidx
+
+        return True if modcnt == 1 else False
 
 
 # This DP solution is TLE
 # This problem can be done in linear time
+# L36 for the case that while loop finished but s and t are in differ length
+# "a" and "ac"
